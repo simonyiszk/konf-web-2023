ff4127729d05a350c82b39aa268166258e7949d7
Andris Borbás <andris.borbas@gmail.com>
2022-12-13 19:43:53 +0100

    Add logo to hero

--- src/components/hero/HeroV1.tsx
+++ src/components/hero/HeroV1.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./HeroV1.module.scss";
@@ -5,10 +6,23 @@ import styles from "./HeroV1.module.scss";
 export function HeroV1() {
 	return (
 		<div className={styles.heroV1}>
-			<h1 className="mx-1 my-8 mt-auto text-center text-5xl font-bold">
-				Hamarosan...
-			</h1>
-			<div className="mt-auto mb-16">
+			<div className="flex h-full flex-col justify-center">
+				<div className="aspect-w-16 aspect-h-7 relative m-4">
+					<Image
+						src="/favicon.svg"
+						alt="XX. Simonyi Konferencia logó"
+						className=""
+						fill
+					/>
+				</div>
+				<h1 className="mx-1 my-8 text-center text-2xl">
+					XX. Simonyi Konferencia
+				</h1>
+				<h2 className="mx-1 my-8 text-center text-5xl font-bold">
+					Hamarosan...
+				</h2>
+			</div>
+			<div className="mb-16">
 				<p className="my-4 text-center text-xl">
 					Addig is tekintsd meg a korábbi konferenciáinkat:
 				</p>
