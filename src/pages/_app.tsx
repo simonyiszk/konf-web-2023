fb3a1d307ae8fffc2fca41f389167b938a6f1491
Andris Borbás <andris.borbas@gmail.com>
2023-01-12 18:30:32 +0100

    Add old service worker purge V2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@/styles/global.scss";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import React from "react";
 
 export default function KonfApp({ Component, pageProps }: AppProps) {
 	return (
 		<React.StrictMode>
+			<Script
+				id="YeetGatsby"
+				dangerouslySetInnerHTML={{
+					__html: `if(window.navigator && navigator.serviceWorker) {
+  												navigator.serviceWorker.getRegistrations()
+  												.then(function(registrations) {
+    												for(let registration of registrations) {
+      												registration.unregister();
+    											}
+  										});
+											}`,
+				}}
+			/>
 			<Component {...pageProps} />
 		</React.StrictMode>
 	);
