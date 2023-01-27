8f51d3932087bc8ded5afd309f1f925f297ea35e
Miklos Daniel <daniel.miklos0@gmail.com>
2023-01-27 17:27:15 +0100

    more responsive stuff

--- src/components/gallery/GallerySection.tsx
+++ src/components/gallery/GallerySection.tsx
@@ -1,34 +1,113 @@
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-import { ImageTile } from "./ImageTile";
+import { ImageTile, ImageTileProps } from "./ImageTile";
+
+const mockImagesInit: ImageTileProps[] = [
+	{
+		image: "https://placekitten.com/200/286",
+		ordinal: 20,
+	},
+	{
+		image: "https://placekitten.com/200/285",
+		ordinal: 19,
+	},
+	{
+		image: "https://placekitten.com/200/284",
+		ordinal: 18,
+	},
+	{
+		image: "https://placekitten.com/200/283",
+		ordinal: 17,
+	},
+	{
+		image: "https://placekitten.com/200/282",
+		ordinal: 16,
+	},
+	{
+		image: "https://placekitten.com/200/281",
+		ordinal: 15,
+	},
+	{
+		image: "https://placekitten.com/200/280",
+		ordinal: 14,
+	},
+	{
+		image: "https://placekitten.com/200/279",
+		ordinal: 13,
+	},
+	{
+		image: "https://placekitten.com/200/278",
+		ordinal: 12,
+	},
+	{
+		image: "https://placekitten.com/200/277",
+		ordinal: 11,
+	},
+	{
+		image: "https://placekitten.com/200/276",
+		ordinal: 10,
+	},
+	{
+		image: "https://placekitten.com/200/275",
+		ordinal: 9,
+	},
+	{
+		image: "https://placekitten.com/200/274",
+		ordinal: 8,
+	},
+	{
+		image: "https://placekitten.com/200/273",
+		ordinal: 7,
+	},
+	{
+		image: "https://placekitten.com/200/272",
+		ordinal: 6,
+	},
+];
 
 export function GallerySection() {
 	const { t } = useTranslation("common");
+
+	// TODO: get images from contentful
+	const images: ImageTileProps[] = mockImagesInit;
+
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const handleSelect = (index: number) => {
+		setActiveIndex(index);
+	};
+
 	return (
 		<div>
-			<section className="mx-auto max-w-6xl py-16">
+			<section className="mx-auto w-full py-16">
 				<h2 className="mb-16 text-center text-4xl">
-					{t("gallery.items.video.title")}
+					{t("gallery.items.photo.title")}
 				</h2>
 				<div className="relative h-full w-full">
 					<ScrollContainer
 						vertical={false}
 						hideScrollbars
 						horizontal
-						className="container flex h-full w-full shrink-0 grow-0 cursor-grab gap-6 overflow-x-hidden"
+						className="flex h-full w-full cursor-grab gap-6"
 					>
-						<ImageTile ordinal={19} />
-						<ImageTile ordinal={18} />
-						<ImageTile ordinal={17} />
-						<ImageTile ordinal={16} />
-						<ImageTile ordinal={15} />
-						<ImageTile ordinal={14} />
-						<ImageTile ordinal={13} />
+						{images.map((image, i) => (
+							<ImageTile
+								key={image.ordinal}
+								{...image}
+								active={activeIndex === i}
+								onClick={() => handleSelect(i)}
+							/>
+						))}
 					</ScrollContainer>
-					<div className="-mt-3.5 h-1 w-full bg-white" />
-					<div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-10 select-none bg-gradient-to-l from-transparent to-konf-background-blue" />
-					<div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-10 select-none bg-gradient-to-r from-transparent to-konf-background-blue" />
+					{images.length > 0 && (
+						<>
+							<div className="-mt-3.5 h-1 w-full bg-white" />
+							<div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-20 select-none bg-gradient-to-l from-transparent via-konf-background-blue/20 to-konf-background-blue" />
+							<div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-20 select-none bg-gradient-to-r from-transparent via-konf-background-blue/20 to-konf-background-blue" />
+						</>
+					)}
 				</div>
 			</section>
 		</div>
--- src/components/gallery/ImageTile.tsx
+++ src/components/gallery/ImageTile.tsx
@@ -1,38 +1,62 @@
+import clsx from "clsx";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 
 import { romanize } from "@/utils/convert";
 
-type ImageTileProps = {
+export type ImageTileProps = {
 	image: string;
 	active?: boolean;
 	ordinal: number;
+	onClick?: () => void;
 };
 
 export function ImageTile({
 	image = "https://placekitten.com/200/300",
 	ordinal,
 	active = false,
+	onClick,
 }: ImageTileProps) {
 	const { t } = useTranslation("common");
 	return (
-		<div>
+		// eslint-disable-next-line jsx-a11y/click-events-have-key-events
+		<div
+			onClick={() => {
+				onClick?.();
+			}}
+			tabIndex={0}
+			role="button"
+			onKeyDown={(e) => {
+				if (e.key === " " || e.key === "Enter") {
+					e.preventDefault();
+					onClick?.();
+				}
+			}}
+		>
 			<div
-				className=" h-60 w-48 shrink-0 select-none overflow-hidden rounded-[4px] border-2"
+				className={clsx(
+					active &&
+						"bg-gradient-to-r from-konf-primary-blue to-konf-primary-green",
+					!active && "border-2",
+					"h-40 w-32 shrink-0 select-none overflow-hidden rounded-[4px] lg:h-60 lg:w-48",
+				)}
 				draggable={false}
 				onDragStart={(e) => e.preventDefault()}
 			>
-				<div className="relative h-40 w-full ">
+				<div className="relative h-24 w-full lg:h-40 ">
 					<Image
 						src={image}
 						alt="íyasd"
 						fill
-						className="select-none object-cover"
+						className={clsx(
+							!active && "grayscale",
+							"select-none object-cover transition-all duration-300 ease-in-out hover:grayscale-[50%]",
+						)}
 						draggable={false}
 					/>
 				</div>
-				<div className="flex h-20 flex-col items-center justify-center">
-					<span className="block text-center text-3xl font-black">{`${romanize(
+				<div className="flex h-16 flex-col items-center justify-center lg:h-20">
+					<span className="block text-center text-xl font-black lg:text-3xl">{`${romanize(
 						ordinal,
 					)}.`}</span>
 					<span className="block text-center">
@@ -41,8 +65,8 @@ export function ImageTile({
 				</div>
 			</div>
 			<div className="flex flex-col items-center justify-center">
-				<span className="mt-4 block w-full text-center text-3xl">
-					{ordinal}
+				<span className="mt-4 block w-full select-none text-center text-3xl">
+					{ordinal + 2003}
 				</span>
 				<div className="flex h-6 w-1 bg-white" />
 			</div>
