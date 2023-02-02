d875c11618878660667a50befecce9150c1d6e5c
András Borbás <andras.borbas@neocoregames.com>
2023-02-02 15:37:39 +0100

    Add new type

--- src/@types/generated/TypeGalleryImages.ts
+++ src/@types/generated/TypeGalleryImages.ts
@@ -3,6 +3,7 @@ import type * as Contentful from "contentful";
 export interface TypeGalleryImagesFields {
     name: Contentful.EntryFields.Symbol;
     images: Contentful.Asset[];
+    thumbnail: Contentful.Asset;
 }
 
 export type TypeGalleryImages = Contentful.Entry<TypeGalleryImagesFields>;
