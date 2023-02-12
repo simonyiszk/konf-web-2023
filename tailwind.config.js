f1aa35d754493d7ea2d5c10ded44484eef5b5ecc
Miklos Daniel <daniel.miklos0@gmail.com>
2023-02-12 23:12:20 +0100

    title gradient animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
 	content: ["./src/**/*.{js,ts,tsx,scss,mdx}"],
 	theme: {
 		extend: {
+			animation: {
+				text: "text 3s ease infinite",
+			},
+			keyframes: {
+				text: {
+					"0%, 100%": {
+						"background-size": "200% 200%",
+						"background-position": "left center",
+					},
+					"50%": {
+						"background-size": "200% 200%",
+						"background-position": "right center",
+					},
+				},
+			},
 			transitionTimingFunction: {
 				DEFAULT: transitionTimingFunction.out,
 			},
