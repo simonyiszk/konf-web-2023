27407aa0a5d1ea291121e4239627ab1d926ae803
Miklos Daniel <daniel.miklos0@gmail.com>
2023-02-14 13:25:12 +0100

    add slug type

--- src/@types/generated/TypePresentation.ts
+++ src/@types/generated/TypePresentation.ts
@@ -1,7 +1,7 @@
 import type * as Contentful from "contentful";
 
 export interface TypePresentationFields {
-    room: "IB025" | "IB028";
+    room?: "IB025" | "IB028";
     title: Contentful.EntryFields.Symbol;
     name: Contentful.EntryFields.Symbol;
     profession: Contentful.EntryFields.Symbol;
@@ -10,6 +10,7 @@ export interface TypePresentationFields {
     image: Contentful.Asset;
     description: Contentful.EntryFields.Text;
     videoLink?: Contentful.EntryFields.Symbol;
+    slug: Contentful.EntryFields.Symbol;
 }
 
 export type TypePresentation = Contentful.Entry<TypePresentationFields>;
