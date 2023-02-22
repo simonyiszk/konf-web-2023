f3ebec3bb969f145f31b090d8dfa515436d76b78
András Borbás <andras.borbas@neocoregames.com>
2023-02-22 11:11:31 +0100

    Update deps

--- src/@types/generated/TypeParagraph.ts
+++ src/@types/generated/TypeParagraph.ts
@@ -1,14 +1,14 @@
-import type * as Contentful from "contentful";
+import type { Asset, Entry, EntryFields } from "contentful";
 import type { LocalizedEntry, LocalizedFields } from "./Localized";
 
 export interface TypeParagraphFields {
-    name: Contentful.EntryFields.Symbol;
-    content: Contentful.EntryFields.Text;
-    image?: Contentful.Asset;
-    video?: Contentful.EntryFields.Symbol;
+    name: EntryFields.Symbol;
+    content: EntryFields.Text;
+    image?: Asset;
+    video?: EntryFields.Symbol;
     mediaSide?: "left" | "right";
 }
 
-export type TypeParagraph = Contentful.Entry<TypeParagraphFields>;
+export type TypeParagraph = Entry<TypeParagraphFields>;
 export type LocalizedTypeParagraphFields<Locales extends keyof any> = LocalizedFields<TypeParagraphFields, Locales>;
 export type LocalizedTypeParagraph<Locales extends keyof any> = LocalizedEntry<TypeParagraph, Locales>;
--- src/@types/generated/TypePresentation.ts
+++ src/@types/generated/TypePresentation.ts
@@ -1,21 +1,21 @@
-import type * as Contentful from "contentful";
+import type { Asset, Entry, EntryFields } from "contentful";
 import type { LocalizedEntry, LocalizedFields } from "./Localized";
 import type { TypeSponsorLogoFields } from "./TypeSponsorLogo";
 
 export interface TypePresentationFields {
     room?: "IB025" | "IB028";
-    title: Contentful.EntryFields.Symbol;
-    name: Contentful.EntryFields.Symbol;
-    profession: Contentful.EntryFields.Symbol;
-    startDate: Contentful.EntryFields.Date;
-    endDate: Contentful.EntryFields.Date;
-    image: Contentful.Asset;
-    description: Contentful.EntryFields.Text;
-    videoLink?: Contentful.EntryFields.Symbol;
-    slug: Contentful.EntryFields.Symbol;
-    sponsorLogo?: Contentful.Entry<TypeSponsorLogoFields>;
+    title: EntryFields.Symbol;
+    name: EntryFields.Symbol;
+    profession: EntryFields.Symbol;
+    startDate: EntryFields.Date;
+    endDate: EntryFields.Date;
+    image: Asset;
+    description: EntryFields.Text;
+    videoLink?: EntryFields.Symbol;
+    slug: EntryFields.Symbol;
+    sponsorLogo?: Entry<TypeSponsorLogoFields>;
 }
 
-export type TypePresentation = Contentful.Entry<TypePresentationFields>;
+export type TypePresentation = Entry<TypePresentationFields>;
 export type LocalizedTypePresentationFields<Locales extends keyof any> = LocalizedFields<TypePresentationFields, Locales>;
 export type LocalizedTypePresentation<Locales extends keyof any> = LocalizedEntry<TypePresentation, Locales>;
