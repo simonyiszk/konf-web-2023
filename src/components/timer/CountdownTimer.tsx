39cc98ba39ccfb81098c14be8d59abe553075312
Andris Borbás <andris.borbas@gmail.com>
2023-02-23 03:21:02 +0100

    Possibly fix hydration errors

--- src/components/timer/CountdownTimer.tsx
+++ src/components/timer/CountdownTimer.tsx
@@ -69,7 +69,10 @@ export function CountdownTimer({ endDate }: SeasonTimerProps) {
 	const { t } = useTranslation("common");
 
 	const [time, setTime] = useState(
-		timeBetween(Date.now(), Date.parse(endDate)),
+		timeBetween(
+			Date.parse("2023-01-01T08:00:00.000+02:00"),
+			Date.parse(endDate),
+		),
 	);
 
 	// Update the time first render and every minute
