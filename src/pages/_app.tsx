ba44693870ea281da327291f64c7624ca5014efe
Miklos Daniel <daniel.miklos0@gmail.com>
2023-02-23 13:09:11 +0100

    fade between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/global.scss";
 
+import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import { appWithTranslation } from "next-i18next";
@@ -28,8 +29,9 @@ function KonfApp({ Component, pageProps }: AppProps) {
 					__html: `if(window.navigator && navigator.serviceWorker){navigator.serviceWorker.getRegistrations().then(function(registrations){for(let registration of registrations){registration.unregister();}});}`,
 				}}
 			/>
-
-			<Component {...pageProps} />
+			<AnimatePresence mode="wait" initial={false}>
+				<Component {...pageProps} />
+			</AnimatePresence>
 		</React.StrictMode>
 	);
 }
