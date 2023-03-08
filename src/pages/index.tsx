50c174d3c0be74b1ca944b3d111afbee68fd123c
Miklos Daniel <daniel.miklos0@gmail.com>
2023-03-08 22:45:45 +0100

    charles section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import type { Asset } from "contentful";
 import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useEffect, useState } from "react";
 
-import type { TypePresentationFields } from "@/@types/generated";
+import type {
+	LocalizedEntry,
+	LocalizedFields,
+	TypePresentationFields,
+} from "@/@types/generated";
 import { GallerySection } from "@/components/gallery/GallerySection";
 import { GiveawaySection } from "@/components/giveaway/GiveawaySection";
 import { HeroV1 } from "@/components/hero/HeroV1";
@@ -11,10 +16,12 @@ import { BgDecoration } from "@/components/layout/decorations/BgDecoration";
 import { Layout } from "@/components/layout/Layout";
 import { Seo } from "@/components/layout/Seo";
 import { PresentationSection } from "@/components/presentations/PresentationSection";
+import { CharlesSection } from "@/components/section/CharlesSection";
 import { SponsorSection } from "@/components/sponsors/SponsorSection";
 import { VideoSection } from "@/components/video/VideoSection";
 import {
 	getGalleryImages,
+	getPresentation,
 	getPresentations,
 	getSponsors,
 	ReturnTypePresentations,
@@ -27,6 +34,21 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 	const galleryAlbums = await getGalleryImages();
 	const presentations = (await getPresentations()) as ReturnTypePresentations;
 
+	const charlesSimonyiPresentation = (
+		await getPresentation({
+			presenter: "Charles Simonyi",
+		})
+	).fields as unknown as LocalizedFields<TypePresentationFields, "hu">;
+
+	const localizedCharlesSimonyiPresentation = Object.fromEntries(
+		Object.entries(charlesSimonyiPresentation).map(([key, value]) => [
+			key,
+			value.hu,
+		]),
+	) as unknown as Omit<TypePresentationFields, "image"> & {
+		image: LocalizedEntry<Asset, "en" | "hu">;
+	};
+
 	return {
 		props: {
 			...i18n,
@@ -35,6 +57,7 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 			galleryAlbums,
 			buildDate: Date.now(),
 			presentations,
+			localizedCharlesSimonyiPresentation,
 		},
 	};
 }
@@ -47,6 +70,7 @@ export default function Index({
 	galleryAlbums,
 	sponsors,
 	presentations,
+	localizedCharlesSimonyiPresentation,
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	...props
 }: PageProps) {
@@ -97,6 +121,7 @@ export default function Index({
 			<Seo />
 
 			<PresentationSection presentations={localizedPresentations} />
+			<CharlesSection {...localizedCharlesSimonyiPresentation} />
 			<VideoSection videoId={videoId} />
 			<GiveawaySection />
 
