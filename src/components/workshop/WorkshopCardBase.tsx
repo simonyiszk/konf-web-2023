1f899717f4a2030a3c5ca2e37ecf3942b51565df
András Borbás <andras.borbas@neocoregames.com>
2023-03-10 11:52:53 +0100

    Fix workshop image size 1

--- src/components/workshop/WorkshopCardBase.tsx
+++ src/components/workshop/WorkshopCardBase.tsx
@@ -20,7 +20,7 @@ export function WorkshopCardBase({
 	return (
 		<div className="overflow-hidden rounded-2xl bg-white/10 backdrop-blur">
 			<WorkshopHeader {...header} />
-			<div className="grid sm:grid-cols-3">
+			<div className="relative grid sm:grid-cols-3">
 				<div
 					className={clsx(
 						"order-2 my-4 px-8 py-10 sm:order-1 sm:my-0",
@@ -30,7 +30,7 @@ export function WorkshopCardBase({
 					{children}
 				</div>
 				{fullSizedImage && (
-					<div className="relative order-1 mt-8 aspect-1 h-full w-full border-white sm:order-2 sm:my-0 sm:border-l-[3px] ">
+					<div className="aspect-w-1 aspect-h-1 relative order-1 mt-8 h-full w-full border-white sm:order-2 sm:my-0 sm:border-l-[3px]">
 						<Image
 							src={fullSizedImage.src}
 							alt={fullSizedImage.alt}
