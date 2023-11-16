107eb7e6fb3501f2bd041c6caca2025cb9772a74
Trisztán Piller <piller.trisztan@simonyi.bme.hu>
2023-11-16 20:16:31 +0100

    Coming soon page

---
+++ src/pages/coming-soon.tsx
@@ -0,0 +1,11 @@
+export default function ComingSoon() {
+	return (
+		<div className="flex h-screen">
+			<div className="m-auto text-center">
+				<p className="mb-12 text-4xl sm:text-6xl">🚧 Hamarosan 🚧</p>
+				<h1 className="text-2xl">XXI. Simonyi Konferencia</h1>
+				<h2 className="my-3 text-xl">2024. március 19.</h2>
+			</div>
+		</div>
+	);
+}
